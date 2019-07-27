ac7e6c5c35cfc058dd052c0ff6c10df5140e356c
Deybis Melendez <40205683+DeybisMelendez@users.noreply.github.com>
2019-07-27 17:42:36 -0600

    Update vector.lua

--- vector.lua
+++ vector.lua
@@ -105,7 +105,7 @@ local mt = { -- Metatable of vector
         function vec:round(dec)
             dec = dec or 0
             local mult = 10^(dec)
-            local x, y = 0, 0
+            local x, y
             if self.x >= 0 then x = math.floor(self.x * mult + 0.5) / mult
             else x = math.ceil(self.x * mult - 0.5) / mult end
             if self.y >= 0 then y = math.floor(self.y * mult + 0.5) / mult
