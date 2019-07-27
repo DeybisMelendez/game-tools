e72cc3ea5e46d510fd78e79471ea86769c3aad6f
Deybis Melendez <40205683+DeybisMelendez@users.noreply.github.com>
2019-07-27 17:28:20 -0600

    Update vector_spec.lua

--- spec/vector_spec.lua
+++ spec/vector_spec.lua
@@ -1,6 +1,6 @@
 local vector = require "vector"
 
-describe("vector", function() 
+describe("vector", function()
   local vec = vector(2,3)
   it("Adds", function() assert.equal(5, vec.x + vec.y) end)
 end)
